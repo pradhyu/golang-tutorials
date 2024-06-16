9b54dca542762ca6df0ec7d80b327a5f63382627
Pkshrestha <pradhyu.shrestha@gmail.com>
2024-06-16 11:40:30 -0400

    hello to hello serve over rest

--- 01-hello-world/main.go
+++ 01-hello-world/main.go
@@ -1,8 +1,116 @@
-// My first Program
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+type Item struct {
+	ID    int    `json:"id"`
+	Name  string `json:"name"`
+	Price int    `json:"price"`
+}
+
+var items []Item
 
 func main() {
-	fmt.Println("Hello, World")
+	router := mux.NewRouter()
+
+	// Define the endpoints for CRUD operations
+	router.HandleFunc("/items", getItems).Methods("GET")
+	router.HandleFunc("/items/{id}", getItem).Methods("GET")
+	router.HandleFunc("/items", createItem).Methods("POST")
+	router.HandleFunc("/items/{id}", updateItem).Methods("PUT")
+	router.HandleFunc("/items/{id}", deleteItem).Methods("DELETE")
+
+	// Start the HTTP server
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func getItems(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(items)
+}
+
+func getItem(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+
+	id, err := strconv.Atoi(params["id"])
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	for _, item := range items {
+		if item.ID == id {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+
+	http.NotFound(w, r)
+}
+
+func createItem(w http.ResponseWriter, r *http.Request) {
+	var item Item
+	err := json.NewDecoder(r.Body).Decode(&item)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	item.ID = len(items) + 1
+	items = append(items, item)
+
+	json.NewEncoder(w).Encode(item)
+}
+
+func updateItem(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+
+	id, err := strconv.Atoi(params["id"])
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	var updatedItem Item
+	err = json.NewDecoder(r.Body).Decode(&updatedItem)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	for i, item := range items {
+		if item.ID == id {
+			items[i] = updatedItem
+			json.NewEncoder(w).Encode(updatedItem)
+			return
+		}
+	}
+
+	http.NotFound(w, r)
+}
+
+func deleteItem(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+
+	id, err := strconv.Atoi(params["id"])
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	for i, item := range items {
+		if item.ID == id {
+			items = append(items[:i], items[i+1:]...)
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+	}
+
+	http.NotFound(w, r)
 }
